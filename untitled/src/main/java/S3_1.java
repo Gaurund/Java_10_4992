edbcc006f05c89d0817b6e7d090bb72f4027a656
Gaurund <pavel.makushev@gmail.com>
2023-03-15 00:27:10 +0300

    Some changes

--- untitled/src/main/java/S3_1.java
+++ untitled/src/main/java/S3_1.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class S3_1 {
+import java.util.Arrays;
+import java.util.List;
+
+public class S3_1 {
+    public static void main(String[] args) {
+        String day = "13";
+        String month = "3";
+        String year = "2013";
+        String[] date = {day, month, year};
+        List<String> d = Arrays.asList(date);
+        System.out.println(d);
+        date[1] = "4";
+        System.out.println(d);
+
+
+    }
 }
